7eeecc8dda6052d21e8dbce1331f16f681de75a3
pparulski <parulski.pp@gmail.com>
2025-02-09 16:40:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = createRoot(rootElement);
 root.render(
-  <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
