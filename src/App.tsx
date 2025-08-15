5d91e9b0cbe54b3f2947814410644e43ed642a2f
pparulski <piotr.parulski@mta.digital>
2025-08-15 15:20:58 +0200

    add RMU

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { RM } from '@request-metrics/browser-agent';
+RM.install({
+    token: "x8eq6hq:m8wk2cx"
+});
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from 'react-helmet-async';
 import { Layout } from "@/components/Layout";
